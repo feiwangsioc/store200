42000557e8f28b93ae0a8161e65634074e6744c1
Fei Wang <feiwangsioc@Feis-MBP.fios-router.home>
2020-02-17 09:09:19 -0500

    init

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+
+  resources :products do 
+  	member do 
+  		post :add_to_cart
+  	end 
+  end 
+
+  root 'products#index'
+  
+  namespace :admin do 
+  	resources :products
+  end 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
